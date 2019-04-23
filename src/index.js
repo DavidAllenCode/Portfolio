0d6bd2b74e98ac4dfb32abaeeb22e6269d42c978
David Allen <dallen1@unum.com>
2019-04-23 10:22:09 -0400

    Basic changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import WebFont from "webfontloader";
+import "../src/styles/css/style.css";
+import Router from "./components/Router/Router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+WebFont.load({
+  google: {
+    families: ["Open Sans:300,400,600,700", "sans-serif"]
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Router />, document.getElementById("davidAllen"));
